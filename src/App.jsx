9f3871dc32250ad4d5df68fefd614644d67500fc
Curt <curt6815463@gmail.com>
2016-08-18 23:25:09 +0000

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ export class App extends React.Component {
       <div>
         React Example
         <div>
-          
+          <Link to='/counter'>counter page</Link>
+          <Link to='/about/curt'>about</Link>
         </div>
         {this.props.children}
       </div>
